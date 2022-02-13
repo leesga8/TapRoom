60844cc2e2e792cd2369c293983cdb9ef03e8ac8
Albert Lee <leesga8@gmail.com>
2022-02-13 15:10:26 -0800

    add Header component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import Header from "./Header";
+
+function App(){
+  return (
+    <React.Fragment>
+      <Header />
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default App;
